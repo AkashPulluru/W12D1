1b51f19d189fba6ba270654ebef84c3c548fc423
Akash Pulluru <akashpulluru@gmail.com>
2023-11-06 14:24:56 -0500

    folder done

--- practice-for-week-15-react-refactoring-class-components-long-practice-main/practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Folder.js
+++ practice-for-week-15-react-refactoring-class-components-long-practice-main/practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Folder.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useState } from 'react';
 
+ 
 const Headers = ({ titles, currentTab, selectTab }) => {
   const handleClick = (e) => {
     const idx = parseInt(e.target.id, 10);
@@ -28,21 +30,14 @@ const Headers = ({ titles, currentTab, selectTab }) => {
   );
 }
 
-class Folder extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentTab: 0
-    };
+const Folder = ({folders}) => { 
+  const [currentTab, setCurrentTab] = useState(0)
+  const selectTab = (num) => {
+    setCurrentTab(num);
   }
-  
-  selectTab = (num) => {
-    this.setState({ currentTab: num });
-  }
-  
-  render() {
-    const folder = this.props.folders[this.state.currentTab];
-    const titles = this.props.folders.map((folder) => folder.title);
+
+    const folder = folders[currentTab];
+    const titles = folders.map((folder) => folder.title);
     
     return (
       <section className="tabs-section">
@@ -50,8 +45,8 @@ class Folder extends React.Component {
         <div className='tabs'>
           <Headers
             titles={titles}
-            currentTab={this.state.currentTab}
-            selectTab={this.selectTab}
+            currentTab={currentTab}
+            selectTab={selectTab}
           />
           <div className='tab-content'>
             {folder.content}
@@ -59,7 +54,38 @@ class Folder extends React.Component {
         </div>
       </section>
     );
-  }
 }
+
+// class Folder extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       currentTab: 0
+//     };
+//   }
+  
+ 
+  
+//   render() {
+//     const folder = this.props.folders[this.state.currentTab];
+//     const titles = this.props.folders.map((folder) => folder.title);
+    
+//     return (
+//       <section className="tabs-section">
+//         <h1>Tabs</h1>
+//         <div className='tabs'>
+//           <Headers
+//             titles={titles}
+//             currentTab={this.state.currentTab}
+//             selectTab={this.selectTab}
+//           />
+//           <div className='tab-content'>
+//             {folder.content}
+//           </div>
+//         </div>
+//       </section>
+//     );
+//   }
+// }
 
 export default Folder;
